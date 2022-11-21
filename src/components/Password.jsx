eac1d44db802f3264a7ba96a56b2a69107b30f06
BrownCarter <lilbrown3000@gmail.com>
2022-11-21 19:52:36 +0100

    changes: many state to sing let state for event fields

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -3,12 +3,10 @@ import { FaRegCopy } from 'react-icons/fa'
 import { FaArrowRight } from 'react-icons/fa'
 function Password() {
     const [range, setRange] = useState(0);
-    const [upper, setUpper] = useState(false);
-    const [lower, setLower] = useState(false);
-    const [number, setNumber] = useState(false);
-    const [symbol, setSymbol] = useState(false);
+    const [data, setData] = useState({upper: false, lower: false, number: false, symbol: false, password: false});
     const [password, setPassword] = useState('');
-
+    
+    const {upper, lower, number, symbol} = data;
     const upperCase = upper ? "ABCDEFGHIJKLMNOPQRSTUVWXYZ" : "";
     const lowerCase = lower ? "abcdefghijklmnopqrstuvwxyz" : "";
     const x_number = number ? "0123456789" : "";
@@ -25,7 +23,14 @@ function Password() {
         }
         setPassword(value);
     }
-
+    
+    const tick = (event) => {
+        setData(prevData => {
+            return {
+                ...prevData, [event.target.name]: !JSON.parse(event.target.value)
+            }
+        })
+    }
     return (
         <div className='flex flex-col p-2 text-white'>
             <h1 className='text-center text-gray-400 font-bold tracking-wider'>Password Generator</h1>
@@ -43,10 +48,10 @@ function Password() {
                     <input onChange={(e) => setRange(e.target.value)} type="range" value={range} min='0' max='23' className='w-full appearance-none bg-black h-2 rounded-lg cursor-pointer outline-none border-none' />
                 </div>
                 <div className='flex flex-col'>
-                    <label className='tracking-widest text-xs flex items-center cursor-pointer' htmlFor='upper' > <input className='outline-none border-none w-5 h-5 cursor-pointer' onChange={() => setUpper((prevUpper) => !prevUpper)} type="checkbox" id="upper" name="" value={upper} />&emsp;Include Uppercase Letters</label><br />
-                    <label className='tracking-widest text-xs flex items-center cursor-pointer' htmlFor='lower' > <input className='outline-none border-none w-5 h-5 cursor-pointer' onChange={() => setLower((prevUpper) => !prevUpper)} type="checkbox" id="lower" name="" value={lower} />&emsp;Include Lowercase Letters</label><br />
-                    <label className='tracking-widest text-xs flex items-center cursor-pointer' htmlFor='number' > <input className='outline-none border-none w-5 h-5 cursor-pointer' onChange={() => setNumber((prevUpper) => !prevUpper)} type="checkbox" id="number" name="" value={number} />&emsp;Include Numbers</label><br />
-                    <label className='tracking-widest text-xs flex items-center cursor-pointer' htmlFor='symbol' > <input className='outline-none border-none w-5 h-5 cursor-pointer' onChange={() => setSymbol((prevUpper) => !prevUpper)} type="checkbox" id="symbol" name="" value={symbol} />&emsp;Include Symbols</label><br />
+                    <label className='tracking-widest text-xs flex items-center cursor-pointer' htmlFor='upper' > <input className='outline-none border-none w-5 h-5 cursor-pointer' onChange={(event) => tick(event)} type="checkbox" id="upper" name="upper" value={upper} />&emsp;Include Uppercase Letters</label><br />
+                    <label className='tracking-widest text-xs flex items-center cursor-pointer' htmlFor='lower' > <input className='outline-none border-none w-5 h-5 cursor-pointer' onChange={(event) => tick(event)} type="checkbox" id="lower" name="lower" value={lower} />&emsp;Include Lowercase Letters</label><br />
+                    <label className='tracking-widest text-xs flex items-center cursor-pointer' htmlFor='number' > <input className='outline-none border-none w-5 h-5 cursor-pointer' onChange={(event) => tick(event)} type="checkbox" id="number" name="number" value={number} />&emsp;Include Numbers</label><br />
+                    <label className='tracking-widest text-xs flex items-center cursor-pointer' htmlFor='symbol' > <input className='outline-none border-none w-5 h-5 cursor-pointer' onChange={(event) => tick(event)} type="checkbox" id="symbol" name="symbol" value={symbol} />&emsp;Include Symbols</label><br />
                 </div>
                 <div className='my-4 h-10 flex flex-row items-center justify-between p-4 bg-neutral-900'>
                     <p className='font-bold text-neutral-700 tracking-wider'>STRENGTH</p>
